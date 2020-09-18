0765934d3bfe9d342be5a2952e246c9725248ad6
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-18 12:47:19 -0700

    add test for update drink

--- src/__tests__/reducers/drink-list-reducer.test.js
+++ src/__tests__/reducers/drink-list-reducer.test.js
@@ -11,6 +11,15 @@ describe('drinkListReducer', ()=>{
     id: 1
   };
 
+  const testDrink2 = {
+    name: 'Test Ale 2',
+    brand: 'Test Brand 2',
+    price: 4.00,
+    alcoholContent: 6.5,
+    quantity: 124,
+    id: 2
+  };
+
   it('should do nothing if given a blank reducer', ()=>{
     expect(drinkListReducer({}, {type: null})).toEqual({});
   });
@@ -34,6 +43,40 @@ describe('drinkListReducer', ()=>{
         alcoholContent,
         quantity,
         id
+      }
+    });
+  });
+
+  it('should be able to update a drink', ()=>{
+    const { name, brand, price, alcoholContent, quantity, id } = testDrink;
+    const { name2, brand2, price2, alcoholContent2, quantity2 } = testDrink2;
+    const action = {
+      type: c.ADD_DRINK,
+      name,
+      brand,
+      price,
+      alcoholContent,
+      quantity,
+      id
+    };
+    const startTestState = drinkListReducer({}, action);
+    const action2 = {
+      type: c.ADD_DRINK,
+      name2,
+      brand2,
+      price2,
+      alcoholContent2,
+      quantity2,
+      id
+    };
+    expect(drinkListReducer(startTestState, action2)).toEqual({
+      1: {
+        name2,
+        brand2,
+        price2,
+        alcoholContent2,
+        quantity2,
+        id: 1
       }
     })
   })
