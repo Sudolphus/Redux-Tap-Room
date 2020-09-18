59d9ca307409740693f3bf4d215706bf96fae747
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-18 13:39:35 -0700

    pass second smoke test for root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import * as a from './../../actions/index';
 import rootReducer from './../../reducers/index';
 import drinkListReducer from './../../reducers/drink-list-reducer';
 
@@ -9,5 +10,19 @@ describe('rootReducer', ()=>{
 
   it('should have an initial state that matches the drink list reducer', ()=>{
     expect(store.getState().drinkList).toEqual(drinkListReducer(undefined, nullAction));
+  })
+
+  it('should update the store in the same was as the drink list reducer', ()=>{
+    const testDrink = {
+      name: 'Test Ale',
+      brand: 'Test Brand',
+      price: 3.00,
+      alcoholContent: 5.5,
+      quantity: 124,
+      id: 1
+    };
+    const testAction = a.addDrink(testDrink);
+    store.dispatch(testAction);
+    expect(store.getState().drinkList).toEqual(drinkListReducer(undefined, testAction));
   })
 })
