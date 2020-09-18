004a605f50d670f46e0bc48bcd300ebc3460d8d0
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-18 13:19:44 -0700

    pass test for addDrink action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,14 @@
 import * as c from './ActionTypes';
 
+export const addDrink = (drink) => {
+  const { name, brand, price, alcoholContent, quantity, id } = drink;
+  return({
+    type: c.ADD_DRINK,
+    name,
+    brand,
+    price,
+    alcoholContent,
+    quantity,
+    id
+  });
+}
