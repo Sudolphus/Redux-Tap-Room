ed318f34d9d62cca5cac937985976e47e27de59f
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-18 13:43:52 -0700

    add redux to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
