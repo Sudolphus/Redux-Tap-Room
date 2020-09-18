54f25383ec97a915f39ea3f1d8f562df2abc3436
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-18 12:37:02 -0700

    add test for add drink

--- src/__tests__/reducers/drink-list-reducer.test.js
+++ src/__tests__/reducers/drink-list-reducer.test.js
@@ -2,7 +2,39 @@ import drinkListReducer from './../../reducers/drink-list-reducer';
 import * as c from './../../actions/ActionTypes';
 
 describe('drinkListReducer', ()=>{
+  const testDrink = {
+    name: 'Test Ale',
+    brand: 'Test Brand',
+    price: 3.00,
+    alcoholContent: 5.5,
+    quantity: 124,
+    id: 1
+  };
+
   it('should do nothing if given a blank reducer', ()=>{
-    expect(drinkListReducer({}, {action: null})).toEqual({});
+    expect(drinkListReducer({}, {type: null})).toEqual({});
+  });
+
+  it('should add a drink to the list', ()=>{
+    const { name, brand, price, alcoholContent, quantity, id } = testDrink;
+    const action = {
+      type: c.ADD_DRINK,
+      name,
+      brand,
+      price,
+      alcoholContent,
+      quantity,
+      id
+    }
+    expect(drinkListReducer({}, action)).toEqual({
+      1: {
+        name,
+        brand,
+        price,
+        alcoholContent,
+        quantity,
+        id
+      }
+    })
   })
 })
