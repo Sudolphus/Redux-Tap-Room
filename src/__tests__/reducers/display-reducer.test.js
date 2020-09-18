7a48780d08167c22995e6853e0a038b3ee83055a
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-18 14:16:47 -0700

    display reducer can switch to the index

--- src/__tests__/reducers/display-reducer.test.js
+++ src/__tests__/reducers/display-reducer.test.js
@@ -1,9 +1,17 @@
 import displayReducer from './../../reducers/display-reducer';
+import * as d from './../../components/DisplayTypes';
 
 describe('displayReducer', ()=>{
   const nullAction = {type: null};
 
   it('should do nothing if null action passed to reducer', ()=>{
     expect(displayReducer({}, nullAction)).toEqual({});
+  });
+
+  it('should be able to redirect to index', ()=>{
+    expect(displayReducer(undefined, d.INDEX)).toEqual({
+      currentPage: d.INDEX,
+      currentDrinkId: null
+    })
   })
 })
--- src/reducers/display-reducer.js
+++ src/reducers/display-reducer.js
@@ -1,6 +1,13 @@
-export default (state = {currentPage: 'index', currentDrinkId: null}, action) => {
+import * as d from './../components/DisplayTypes';
+
+export default (state = {currentPage: d.INDEX, currentDrinkId: null}, action) => {
   const {type} = action;
   switch (type) {
+    case d.INDEX:
+      return {
+        currentPage: d.INDEX,
+        currentDrinkId: null
+      }
     default:
       return state;
   }
